8845efe6a0a136457a5f644b119f360f25507783
kayleerudaina <112531884+kayleerudaina@users.noreply.github.com>
2022-09-06 13:23:28 +0700

    Update settings.py

--- rprojectkaylee/settings.py
+++ rprojectkaylee/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-7l+&=ssmqc&k$jim44kp3s96e6khlynvvg_13d7p8rw+6u_*4k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
